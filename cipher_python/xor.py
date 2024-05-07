e9f79952fcc75a99cd92d585e216e762fd3276d6
utkarsh anand <utkarshanand688@gmail.com>
2024-05-07 22:40:13 +0530

    xor_opertor

--- cipher_python/xor.py
+++ cipher_python/xor.py
@@ -1,44 +1,18 @@
-import tkinter as tk
+def xor_cipher(text,key):
+    if isinstance(text,str):
+        text=text.encode()
 
-def xor_cipher(text, key):
-    if isinstance(text, str):
-        text = text.encode()
+    if len(key)<len(text):
+        key=key*(len(text)//len(key)+1)
+        key=key[:len(text)]
 
-    if len(key) < len(text):
-        key = key * (len(text) // len(key) + 1)
-        key = key[:len(text)]
+    return bytes([x^y for x,y in zip(text,key)])
 
-    return bytes([x ^ y for x, y in zip(text, key)])
+original_text="Hello World! Subscribe to Utkarsh"
 
-def encrypt_decrypt():
-    original_text = text_entry.get()
-    key = key_entry.get().encode()
+key=b'Utkarsh'
+encrypted=xor_cipher(original_text,key)
+print(encrypted)
 
-    encrypted = xor_cipher(original_text, key)
-    decrypted = xor_cipher(encrypted, key)
-
-    result_label.config(text="Encrypted Text: {}\nDecrypted Text: {}".format(encrypted.decode(), decrypted.decode()))
-
-# GUI Setup
-root = tk.Tk()
-root.title("XOR Cipher")
-
-text_label = tk.Label(root, text="Enter Text:")
-text_label.pack()
-
-text_entry = tk.Entry(root)
-text_entry.pack()
-
-key_label = tk.Label(root, text="Enter Key:")
-key_label.pack()
-
-key_entry = tk.Entry(root)
-key_entry.pack()
-
-encrypt_button = tk.Button(root, text="Encrypt / Decrypt", command=encrypt_decrypt)
-encrypt_button.pack()
-
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-root.mainloop()
+decrypted=xor_cipher(encrypted,key)
+print(decrypted.decode())
